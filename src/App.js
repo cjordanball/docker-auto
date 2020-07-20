1dd29d5dde0c122c3c1c9d9d90cc3cb202d4c844
Jordan Ball <jordan.ball@willowtreeapps.com>
2020-07-20 16:47:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Jordan's Big Application!
         </p>
         <a
           className="App-link"
