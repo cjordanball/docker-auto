f3581a7e037aa97b48cff98ae0b9622b918e5c68
Jordan Ball <jordan.ball@willowtreeapps.com>
2020-07-21 10:54:46 -0400

    change size of app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Jordan's Big Application!
+          Jordan's Tiny Application!
         </p>
         <a
           className="App-link"
